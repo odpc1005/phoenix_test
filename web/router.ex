0d2010c1b3ae9ae65fb29cba734909598c811bd7
David <odpc1005@gmail.com>
2017-08-10 07:20:36 -0500

    watch video

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,13 @@ defmodule Rumbl.Router do
     resources "/users", UserController, only: [:index, :show, :new, :create]
     get "/", PageController, :index
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
+  end
+
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
   end
 
   # Other scopes may use custom stacks.
